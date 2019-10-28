c76c26f475f552b94a612fdee220a34e9a326391
Matthew <matthew.97.clark@gmail.com>
2019-10-28 19:05:27 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -9,7 +9,10 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 def assembled_aoh
   aoh = [
     DON_G,
-    JOELLE_VD
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G
     ]
 end
 
